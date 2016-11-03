eeb47200e09eec50f0732ddb768ea5cc9d029f08
mkhonto <jabulani@projectcodex.co>
2016-11-03 10:22:55 +0200

    variable types

---
+++ variables.js
@@ -0,0 +1,11 @@
+var aWorker = "this worker";
+var aNumb = 20; // a number type
+var aDecimal = 2.1; // a number type
+var aBoolean = true;
+var day = new Date();
+//print the variables
+console.log(typeof aWorker);
+console.log(typeof aNumb);
+console.log(typeof aDecimal);
+console.log(typeof aBoolean);
+console.log(typeof Date);
