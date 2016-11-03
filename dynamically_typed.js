c6b39095d74745777e93e5175d235fa8ba663524
mkhonto <jabulani@projectcodex.co>
2016-11-03 11:04:14 +0200

    dynamical types of variables

---
+++ dynamically_typed.js
@@ -0,0 +1,8 @@
+var theString = "is my string";
+ console.log("theString type is : " + typeof theString);
+
+ //change it to a number
+ theStars = 500;
+ // the type of theString is now a number
+ console.log("the variables type changed!");
+ console.log("theString type is : " typeof theString);
