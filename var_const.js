0fa437471257f99d46f871f5ca2d548c8034ce97
mkhonto <jabulani@projectcodex.co>
2016-11-03 09:34:25 +0200

    two ways to declare variables

---
+++ var_const.js
@@ -0,0 +1,11 @@
+//it's content and type can change many times
+var cars = "32";
+//cars = "10";
+console.log(cars);
+
+const we = "30";
+//we = "2";
+console.log(we);
+//once defined cannot change
+we = "55";
+console.log(we)
