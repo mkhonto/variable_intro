70b860e05230ee207ee4fe8f13cfefa6549cf0c6
mkhonto <jabulani@projectcodex.co>
2016-11-03 11:39:44 +0200

    variable declared, printed and called once more

---
+++ type_errors.js
@@ -0,0 +1,6 @@
+var theCommu = "unity feeds to emancipation"; // here i declared a string
+console.log(theCommu.length); // i going to print the length of a string
+
+theCommu = 456; // this string is changed to a number
+console.log("theCommu.length is now undefined and your program might break!"); // this will fail
+console.log(theCommu.length);
